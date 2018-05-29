59cdeb1a7551325ec7990003ffc14ab27ca071df
Minkyu Ham <minkyuham@Minkyus-MacBook-Pro.local>
2018-05-30 11:45:31 +1200

    YARGS set, users can now enter their address to fetch Lat, LNG

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const REQUEST = require('request');
+const YARGS = require('yargs')
+
+const argv = YARGS.options({
+    a:{
+        demand: true,
+        alias: 'address',
+        describe: 'Address to fetch from GOOGLE API',
+        string: true
+    }
+})
+.help()
+.alias('help', 'h') 
+.argv;
+
+let userURIAddressComponent = encodeURIComponent(argv.address)
+
 
 REQUEST({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=%201/11%20pannill%20place',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${userURIAddressComponent}`,
     json: true
     }, 
     (error, response, body) =>{
-    console.log(JSON.stringify(body, undefined, 2));
+    console.log(`Address:  ${body.results[0].formatted_address}`);
+    console.log(`Geolocation (Latitutde): ${body.results[0].geometry.location.lat}`);
+    console.log(`Geolocation (Longitutde): ${body.results[0].geometry.location.lng}`);
 })
